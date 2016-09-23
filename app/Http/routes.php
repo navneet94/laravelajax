7b7472b52d35ff2c0fb2289f1d26f9e52c77962f
navneet94 <navneet_prakash_singh@live>
2016-09-23 16:03:07 +0530

    Project to demonstrate ajax in laravel
    Project to demonstrate ajax in laravel

---
+++ app/Http/routes.php
@@ -0,0 +1,49 @@
+<?php
+use Illuminate\Http\Request;
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    $tasks = App\Task::all();
+
+    return View::make('welcome')->with('tasks',$tasks);
+});
+
+Route::get('/tasks/{task_id?}',function($task_id){
+    $task = App\Task::find($task_id);
+
+    return Response::json($task);
+});
+
+Route::post('/tasks',function(Request $request){
+    $task = App\Task::create($request->all());
+
+    return Response::json($task);
+});
+
+Route::put('/tasks/{task_id?}',function(Request $request,$task_id){
+    $task = App\Task::find($task_id);
+
+    $task->task = $request->task;
+    $task->description = $request->description;
+
+    $task->save();
+
+    return Response::json($task);
+});
+
+Route::delete('/tasks/{task_id?}',function($task_id){
+    $task = App\Task::destroy($task_id);
+
+    return Response::json($task);
+});
